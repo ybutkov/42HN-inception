1c34909064c63eeeb485be8e3f3bb2cd2c6eb0d6
Yurii Butkov <herr.butkov@gmail.com>
2026-08-02 18:10:37 +0200

    Add configuration for admin to Portainer

---
+++ srcs/requirements/bonus/portainer/tools/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -f "$PORTAINER_PASSWORD_FILE" ]; then
+    PORTAINER_PASS=$(cat "$PORTAINER_PASSWORD_FILE" | tr -d '\r\n')
+    if [ -n "$PORTAINER_PASS" ]; then
+        HASH=$(htpasswd -nbB admin "$PORTAINER_PASS" | cut -d ":" -f 2)
+    fi
+fi
+
+if [ -z "$HASH" ] ; then
+    echo "WARNING: Secret password not found or empty" >&2
+    exec /usr/local/portainer/portainer "$@"
+else
+    exec /usr/local/portainer/portainer --admin-password="$HASH" "$@"
+fi
+

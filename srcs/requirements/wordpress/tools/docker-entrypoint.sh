56a95edb1022a887ba91b79eed9cc0ed1073930f
Yurii Butkov <herr.butkov@gmail.com>
2026-08-06 22:49:41 +0200

    fix import entrypoint-lib.sh

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 set -e
 
-if [ -f "$WP_DB_PASSWORD_FILE" ]; then
-    DB_PASSWORD=$(cat "$WP_DB_PASSWORD_FILE")
-else
-    echo "ERROR: Secret file $WP_DB_PASSWORD_FILE not found!" >&2
-    exit 1
-fi
+# if [ -f "$WP_DB_PASSWORD_FILE" ]; then
+#     DB_PASSWORD=$(cat "$WP_DB_PASSWORD_FILE")
+# else
+#     echo "ERROR: Secret file $WP_DB_PASSWORD_FILE not found!" >&2
+#     exit 1
+# fi
+DB_PASSWORD=$(read_secret "$WP_DB_PASSWORD_FILE")
 
-if [ -f "$WP_ADMIN_PASSWORD_FILE" ]; then
-    WP_ADMIN_PASSWORD=$(cat "$WP_ADMIN_PASSWORD_FILE")
-else
-    echo "ERROR: Secret file $WP_ADMIN_PASSWORD_FILE not found!" >&2
-    exit 1
-fi
+# if [ -f "$WP_ADMIN_PASSWORD_FILE" ]; then
+#     WP_ADMIN_PASSWORD=$(cat "$WP_ADMIN_PASSWORD_FILE")
+# else
+#     echo "ERROR: Secret file $WP_ADMIN_PASSWORD_FILE not found!" >&2
+#     exit 1
+# fi
+WP_ADMIN_PASSWORD=$(read_secret "$WP_ADMIN_PASSWORD_FILE")
 
-if [ -f "$WP_USER_PASSWORD_FILE" ]; then
-    WP_USER_PASSWORD=$(cat "$WP_USER_PASSWORD_FILE")
-else
-    echo "ERROR: Secret file $WP_USER_PASSWORD_FILE not found!" >&2
-    exit 1
-fi
+# if [ -f "$WP_USER_PASSWORD_FILE" ]; then
+#     WP_USER_PASSWORD=$(cat "$WP_USER_PASSWORD_FILE")
+# else
+#     echo "ERROR: Secret file $WP_USER_PASSWORD_FILE not found!" >&2
+#     exit 1
+# fi
+WP_USER_PASSWORD=$(read_secret "$WP_USER_PASSWORD_FILE")
 
-if [ -z "$DB_PASSWORD" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$WP_USER_PASSWORD" ]; then
-    echo "ERROR: One or more secret files are empty!" >&2
-    exit 1
-fi
+# if [ -z "$DB_PASSWORD" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$WP_USER_PASSWORD" ]; then
+#     echo "ERROR: One or more secret files are empty!" >&2
+#     exit 1
+# fi
 
 cd /var/www/html
 
